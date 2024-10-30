6ed4b10fa5604a765306119121c91e89d44e3b6a
arigree <arigree@iu.edu>
2024-10-30 10:42:54 -0400

    styling reset

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
-import { Inter } from "next/font/google";
+
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -10,6 +9,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <section></section>
+    <html lang="en">
+      <body>{children}</body>
+    </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+"use client";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main>
-      
+      <h1>Home</h1>
     </main>
   );
 }
