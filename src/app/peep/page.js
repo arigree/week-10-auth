2ec73a8b509cd85265e7ceb07cb1d403dd754055
arigree <arigree@iu.edu>
2024-10-30 10:55:45 -0400

    troubleshooting

--- src/app/peep/page.js
+++ src/app/peep/page.js
@@ -1,25 +1,21 @@
 "use client";
-import { useState} from 'react';
-import { useRouter } from 'next/compat/router';
+import { useState } from "react";
+import Link from "next/link";
 
+export default function PeepPage() {
+  const [peepName, setPeepName] = useState("");
 
+  function changePeepName(e) {
+    setPeepName(e.currentTarget.value);
+  }
 
-export default function PeepPage(){
-    const [peepName, setPeepName] = useState("");
-    const router = useRouter();
-
-    function changePeepName(e){
-        setPeepName(e.currentTarget.value);
-    }
-    function goToPeep(){
-        router.push(`/peeps/${peepName}`);
-        
-    }
-    return(
-        <main>
-            <h1>Peeps</h1>
-            <input value={peepName} onChange={changePeepName}></input>
-            <button>Go to Peep!</button>
-        </main>
-    )
+  return (
+    <main>
+      <h1>Peeps</h1>
+      <input value={peepName} onChange={changePeepName}></input>
+      <Link href={`/peep/${peepName}`}>
+        <button>Go to Peep!</button>
+      </Link>
+    </main>
+  );
 }
