0b5b0dcb51117a970a4fdbc78461071319db6551
arigree <arigree@iu.edu>
2024-10-30 11:31:26 -0400

    sign in out

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,17 @@
 "use client";
-import { SignInButton, UserProfile } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
-      <SignInButton/>
+      <SignedOut>
+        <SignInButton></SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton></UserButton>
+      </SignedIn>
     </main>
   );
 }
