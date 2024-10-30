f76672b7b77fec4a7cb5c3c50aa766b408cddefe
arigree <arigree@iu.edu>
2024-10-30 10:49:02 -0400

    peep content

--- src/app/peep/page.js
+++ src/app/peep/page.js
@@ -1,9 +1,22 @@
 "use client";
+import { useState} from 'react';
+
+
 
 export default function PeepPage(){
+    const [peepName, setPeepName] = useState("");
+
+    function changePeepName(e){
+        setPeepName(e.currentTarget.value);
+    }
+    function goToPeep(){
+        console.log(peepName)
+    }
     return(
         <main>
             <h1>Peeps</h1>
+            <input value={peepName} onChange={changePeepName}></input>
+            <button>Go to Peep!</button>
         </main>
     )
 }
