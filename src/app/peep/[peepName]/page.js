e8ecc626befbb397dc889a9a93b8f8215f8e0545
arigree <arigree@iu.edu>
2024-10-30 11:08:06 -0400

    traits

--- src/app/peep/[peepName]/page.js
+++ src/app/peep/[peepName]/page.js
@@ -1,9 +1,12 @@
 "use client";
 
+import Link from "next/link";
+
 export default function PeepNamePage({ params }){
 return(
     <main>
         <h1>Peep Name:</h1>
+        <Link href={`/peep/${peepName}/hairColor`}>Hair Color</Link>
     </main>
 )
 }
